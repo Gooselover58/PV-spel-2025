0d78c21486c8637b36023c9e0fa519bd331baf3d
Filip Udde Fritze <151173011+FilipUF@users.noreply.github.com>
2025-01-09 11:30:05 +0100

    mining ores script

---
+++ Assets/Scripts/Item_Scripts/Pickaxe_Script.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickaxe_Script : MonoBehaviour
+{   
+    
+    public float PickaxeDamage = -1;
+    public float Health = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Input.GetMouseButtonDown(0))
+        {
+         OreHealth();
+        }
+    }
+    public void OreHealth()
+    {
+        Health -= PickaxeDamage;
+
+        if (Health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
