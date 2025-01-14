7d7d8c35771a141775823f2b3cb6da5b37899405
Spydnel <110169965+Spydnel@users.noreply.github.com>
2025-01-14 15:58:18 +0100

    :3 :3 :3

--- Assets/Scripts/Tiles/WalkerTile.cs
+++ Assets/Scripts/Tiles/WalkerTile.cs
@@ -6,15 +6,24 @@ using UnityEngine.Tilemaps;
 public class WalkerTile : AbstractTile
 {
     private bool IsDebug;
-    public WalkerTile(Vector2Int position, TilePlacer tilePlacer) : base(position, tilePlacer) { }
+    private Vector2Int Direction;
+    public WalkerTile(Vector2Int position, TilePlacer tilePlacer, Vector2Int direction) : base(position, tilePlacer) 
+    {
+        Direction = direction;
+    }
     public override void OnGenerate()
     {
-        TilePlacer.Debugmap.SetTile(new Vector3Int(Position.x, Position.y, 0), Resources.Load<Tile>("Tiles/debug_walker"));
+        RenderTile(Position);
+    }
 
+    private void RenderTile(Vector2Int position)
+    {
+        TilePlacer.Debugmap.SetTile(new Vector3Int(Position.x, Position.y, 0), Resources.Load<Tile>("Tiles/debug_walker"));
     }
 
     public void Step() 
     {
-        
+        Position += Direction;
+        RenderTile(Position);
     }
 }
