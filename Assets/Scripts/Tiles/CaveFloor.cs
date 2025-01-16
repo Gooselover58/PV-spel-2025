ee638d454acc7fcd2231f27765fa083c5f5264c4
Spydnel <110169965+Spydnel@users.noreply.github.com>
2025-01-16 13:51:38 +0100

    Generation

--- Assets/Scripts/Tiles/CaveFloor.cs
+++ Assets/Scripts/Tiles/CaveFloor.cs
@@ -9,7 +9,7 @@ public class CaveFloor : AbstractTile
 
     public override void OnGenerate() 
     {
-        TilePlacer.Groundmap.SetTile(new Vector3Int(Position.x, Position.y, 0), Resources.Load<Tile>("Tiles/cave_floor"));
-        TilePlacer.Overlaymap.SetTile(new Vector3Int(Position.x, Position.y, 0), Resources.Load<Tile>("Tiles/cave_floor_inner"));
+        tilePlacer.Groundmap.SetTile(new Vector3Int(Position.x, Position.y, 0), Resources.Load<Tile>("Tiles/cave_floor"));
+        tilePlacer.Overlaymap.SetTile(new Vector3Int(Position.x, Position.y, 0), Resources.Load<Tile>("Tiles/cave_floor_inner"));
     }
 }
